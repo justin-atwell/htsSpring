176e6504ef733c25b54e5aded54e743cebe9c55a
Justin Atwell <justin.atwell@hedera.com>
2021-11-09 11:06:17 -0600

    master - updates'

--- src/main/java/hedera/starter/controller/AccountController.java
+++ src/main/java/hedera/starter/controller/AccountController.java
@@ -1,41 +1,63 @@
 package hedera.starter.controller;
 
+import com.hedera.hashgraph.sdk.AccountCreateTransaction;
+import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.Client;
-import com.hedera.hashgraph.sdk.HederaStatusException;
-import com.hedera.hashgraph.sdk.account.AccountId;
-import com.hedera.hashgraph.sdk.account.AccountInfo;
-import com.hedera.hashgraph.sdk.account.AccountInfoQuery;
-import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.Transaction;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.TransactionResponse;
+import com.hedera.hashgraph.sdk.TransferTransaction;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RestController
-@RequestMapping(path = "/account")
-public class AccountController {
-    AccountId accountId;
+public final class AccountController {
 
+    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+    private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
 
-    @GetMapping("")
-    public AccountInfo getAccountInfo() throws HederaStatusException {
+    private AccountController() {
+    }
+
+    public static void main(String[] args) throws Exception {
+
         Client client = Client.forTestnet();
-        accountId = AccountId.fromString(Objects.requireNonNull(getEnv().get("OPERATOR_ID")));
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
-        client.setOperator(accountId, Ed25519PrivateKey.fromString(""));
+        PrivateKey userKey = PrivateKey.generate();
 
-        long cost = new AccountInfoQuery().setAccountId(accountId).getCost(client);
+        AccountId userId = new AccountCreateTransaction()
+                .setKey(userKey.getPublicKey())
+                .setInitialBalance(new Hbar(5))
+                .execute(client).getReceipt(client).accountId;
 
-        AccountInfo info = new AccountInfoQuery()
-                .setAccountId(accountId).setQueryPayment(cost + cost / 50).execute(client);
+        System.out.println("Submitting transaction");
+//        TransactionId transactionId = TransactionId.generate(Justin);
 
-        return info;
+        TransferTransaction transaction = new TransferTransaction()
+//            .setTransactionId(transactionId)
+                .addHbarTransfer(userId, new Hbar(-1))
+                .addHbarTransfer(OPERATOR_ID, new Hbar(1))
+                .freezeWith(client)
+                .sign(OPERATOR_KEY);
+
+        byte[] transBytes = transaction.toBytes();
+        // 3rd act send bytes to client
+        // client could either sign the bytes and return signature to 3rd act who submit to hedera
+        // client signs and submits to hedera
+
+        Transaction<?> transaction1 = Transaction.fromBytes(transBytes);
+
+        transaction1.sign(userKey);
+
+        Client client2 = Client.forTestnet();
+        TransactionResponse response = (TransactionResponse) transaction1.execute(client2);
+        System.out.println("Getting receipt");
+
+        // assuming I'm the end user here
+        TransactionReceipt receipt = response.getReceipt(client2);
+//        System.out.println("Transaction id " + transactionId.toString());
     }
-
-    static Dotenv getEnv() {
-        return Dotenv.load();
-    }
-
 }
