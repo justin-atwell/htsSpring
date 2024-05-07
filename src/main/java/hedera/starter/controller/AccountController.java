a91a4a2abc7c1e0546113e933f57a11df6f4536b
Justin Atwell <justin.atwell@swirldslabs.com>
2024-05-07 06:47:13 -0500

    master - chicken scratch

--- src/main/java/hedera/starter/controller/AccountController.java
+++ src/main/java/hedera/starter/controller/AccountController.java
@@ -1,63 +1,63 @@
 package hedera.starter.controller;
 
-import com.hedera.hashgraph.sdk.AccountCreateTransaction;
-import com.hedera.hashgraph.sdk.AccountId;
-import com.hedera.hashgraph.sdk.Client;
-import com.hedera.hashgraph.sdk.Hbar;
-import com.hedera.hashgraph.sdk.PrivateKey;
-import com.hedera.hashgraph.sdk.Transaction;
-import com.hedera.hashgraph.sdk.TransactionReceipt;
-import com.hedera.hashgraph.sdk.TransactionResponse;
-import com.hedera.hashgraph.sdk.TransferTransaction;
-import io.github.cdimascio.dotenv.Dotenv;
+import com.hedera.hashgraph.sdk.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
+import java.util.concurrent.TimeoutException;
 
+@RestController
 public final class AccountController {
 
-    private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
-    private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
+    private static final AccountId OPERATOR_ID = AccountId.fromString("");
+    private static final PrivateKey OPERATOR_KEY = PrivateKey.fromString("");
 
     private AccountController() {
     }
 
-    public static void main(String[] args) throws Exception {
+    @GetMapping("/accounts")
+    public void getEmployee() throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
 
         Client client = Client.forTestnet();
         client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
-        PrivateKey userKey = PrivateKey.generate();
+        TokenCreateTransaction transaction = new TokenCreateTransaction()
+                .setTokenName("CBR")
+                .setTokenSymbol("cbr")
+                .setTreasuryAccountId(OPERATOR_ID)
+                .setInitialSupply(50000)
+                .setAdminKey(OPERATOR_KEY.getPublicKey())
+                .setMaxTransactionFee(new Hbar(30)); //Change the default max transaction fee
 
-        AccountId userId = new AccountCreateTransaction()
-                .setKey(userKey.getPublicKey())
-                .setInitialBalance(new Hbar(5))
-                .execute(client).getReceipt(client).accountId;
+//Build the unsigned transaction, sign with admin private key of the token, sign with the token treasury private key, submit the transaction to a Hedera network
+        TransactionResponse txResponse = transaction.freezeWith(client).sign(OPERATOR_KEY).execute(client);
 
-        System.out.println("Submitting transaction");
-//        TransactionId transactionId = TransactionId.generate(Justin);
+//Request the receipt of the transaction
+        TransactionReceipt receipt = txResponse.getReceipt(client);
 
-        TransferTransaction transaction = new TransferTransaction()
-//            .setTransactionId(transactionId)
-                .addHbarTransfer(userId, new Hbar(-1))
-                .addHbarTransfer(OPERATOR_ID, new Hbar(1))
-                .freezeWith(client)
-                .sign(OPERATOR_KEY);
+//Get the token ID from the receipt
+        TokenId tokenId = receipt.tokenId;
 
-        byte[] transBytes = transaction.toBytes();
-        // 3rd act send bytes to client
-        // client could either sign the bytes and return signature to 3rd act who submit to hedera
-        // client signs and submits to hedera
+        System.out.println("The new token ID is " + tokenId);
 
-        Transaction<?> transaction1 = Transaction.fromBytes(transBytes);
-
-        transaction1.sign(userKey);
-
-        Client client2 = Client.forTestnet();
-        TransactionResponse response = (TransactionResponse) transaction1.execute(client2);
-        System.out.println("Getting receipt");
+//        TransferTransaction transaction = new TransferTransaction()
+//                .addHbarTransfer(userId, new Hbar(-1))
+//                .addHbarTransfer(OPERATOR_ID, new Hbar(1))
+//                .freezeWith(client)
+//                .sign(OPERATOR_KEY);
+//
+//        byte[] transBytes = transaction.toBytes();
+//
+//        Transaction<?> transaction1 = Transaction.fromBytes(transBytes);
+//
+//        transaction1.sign(userKey);
+//
+//        Client client2 = Client.forTestnet();
+//        TransactionResponse response = (TransactionResponse) transaction1.execute(client2);
+//        System.out.println("Getting receipt");
 
         // assuming I'm the end user here
-        TransactionReceipt receipt = response.getReceipt(client2);
+//        TransactionReceipt receipt = response.getReceipt(client2);
 //        System.out.println("Transaction id " + transactionId.toString());
     }
 }
